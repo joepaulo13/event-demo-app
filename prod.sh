396fae7e40a222d3ecf0b74389692a306a0e035d
Emmanuel Benitez <emmanuel.benitez@nix-digital.com>
2022-11-08 15:35:14 +0800

    fix branch sync commit conflict

--- prod.sh
+++ prod.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-git checkout main;git fetch;git pull;git branch -D prod;git checkout -b prod;git cherry-pick 6f7236f2ac504d6784b940d33494e93b04e8cab7;
+git checkout main;git fetch;git pull;git branch -D prod;git checkout -b prod;git cherry-pick ba36997564ae5bc5827a6f0f021761b7e4782944;
